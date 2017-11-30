abc10d2762f44059fa960e9824e3d7759449a80d
Michael Suguitan <PsychoMugs@dhcp-rhodes-153.eduroam.cornell.edu>
2017-11-30 16:59:14 -0500

    added threading and firebase checking based on gpio interrupt

--- blinds_control.py
+++ blinds_control.py
@@ -4,9 +4,25 @@ from simple_server import *
 # motor control stuff
 import motor_control as motorCtrl
 
+# GPIO setup
+import RPi.GPIO as GPIO
+# use gpio pin 4 (pin 7)
+gpio_in = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_in,GPIO.IN)
+
 # firebase
 from firebase import firebase
 fb = firebase.FirebaseApplication('https://gal-9000.firebaseio.com/', None)
+
+blossom_add = '10.148.9.99:5555/s/'
+blinds_state = ''
+
+# sending http requests
+import urllib2
+
+# threading
+import threading
 
 import SimpleHTTPServer
 import SocketServer
@@ -20,18 +36,28 @@ class funHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print self.path
         self.send_response(200)
-        if (self.path == '/up'):
-            motor_move(-1000)
-            fb_put('up')
-        elif (self.path =='/down'):
-            motor_move(1000)
-            fb_put('down')
-        elif (self.path == '/stop'):
-            motor_move(0)
-        return self.path
+        move_blinds(self.path[1:])
+
+def fb_thread():
+    if (GPIO.input(gpio_in)):
+        blinds_state = fb_check()
 
 def fb_put(state):
-    fb.put('gal-9000','blinds',state)
+    fb.put('blinds','state',state)
+
+def fb_check():
+    blinds_cmd = fb.get('blinds','cmd')
+    blinds_state = fb.get('blinds','state')
+    blossom_s = fb.get('blossom','s')
+    blossom_idle = fb.get('blossom','idle')
+
+    move_blinds(blinds_cmd)
+
+    # command blossom
+    urllib2.urlopen(blossom_add+blossom_s+'/'+blossom_idle)
+
+    return blinds_state
+
 
 def check_motor_pos():
     load = motorCtrl.get_load(1)[0]
@@ -45,8 +71,22 @@ def check_motor_pos():
 def motor_move(speed):
     motorCtrl.move_wheel(1, speed)
 
-if __name__ == "__main__":
+def move_blinds(state):
+    if (state == 'up'):
+        motor_move(-1000)
+        # fb_put('up')
+    elif (state =='down'):
+        motor_move(1000)
+        # fb_put('down')
+    elif (state == 'stop'):
+        motor_move(0)
+    fb.put('blinds','state',state)
 
+
+if __name__ == "__main__":
+    blinds_state = fb_check()
+    t = threading.Thread(target=fb_thread)
+    t.start()
     motorHandler = funHandler
     # motorHandler.do_GET = motor_get
     # motorHandler.do_GET = motorHandler.do_GET(simpleSrvr.funHandler, motor_get)
