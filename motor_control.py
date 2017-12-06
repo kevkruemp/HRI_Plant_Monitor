f1cf83ace54b78638c21e5281fb7158113c46467
Michael Suguitan <PsychoMugs@dhcp-rhodes-635.eduroam.cornell.edu>
2017-12-05 22:22:03 -0500

    Trying to fix blinds

--- motor_control.py
+++ motor_control.py
@@ -29,8 +29,9 @@ def move_wheel(motor, speed):
     # motors.enable_torque({motor})
     while (motors.get_moving_speed({motor})<=speed/10):
         motors.set_torque_limit({motor:100})
+        time.sleep(0.2)
         motors.set_moving_speed({motor: speed})
-        time.sleep(0.5)
+        time.sleep(0.2)
 
     # motors.set_torque_limit({motor:100})
     # motors.set_moving_speed({motor: speed})
