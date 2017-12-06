ae8895eac6299fd84eda7df5dbb735c6124a5739
Michael Suguitan <PsychoMugs@dhcp-rhodes-635.eduroam.cornell.edu>
2017-12-05 22:47:58 -0500

    Ignore eval files

--- motor_control.py
+++ motor_control.py
@@ -46,6 +46,8 @@ def move_wheel(motor, speed):
             motors.set_moving_speed({motor: speed})
             time.sleep(0.2)
             load = motors.get_present_load({motor})[0]
+            print motors.get_moving_speed({motor})[0]
+            print load
             # print load
             # load == 100 indicates stalling at top or bottom
             if (abs(abs(load)-96)<2):
