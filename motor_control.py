29e99fcfc26446d8054c93fd4426b8a9d8e13567
Michael Suguitan <PsychoMugs@dhcp-rhodes-635.eduroam.cornell.edu>
2017-12-05 22:31:52 -0500

    Ignore eval files

--- motor_control.py
+++ motor_control.py
@@ -28,11 +28,11 @@ def move_wheel(motor, speed):
     # motors.set_moving_speed({motor: speed})
     # motors.enable_torque({motor})
     while (abs(motors.get_moving_speed({motor})[0])<1):
-        print "Stall"
         motors.set_torque_limit({motor:100})
         time.sleep(0.2)
         motors.set_moving_speed({motor: speed})
         time.sleep(0.2)
+        print motors.get_moving_speed({motor})[0]
 
     # motors.set_torque_limit({motor:100})
     # motors.set_moving_speed({motor: speed})
